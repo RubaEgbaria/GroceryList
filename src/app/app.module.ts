bc0d20d5726356a87fbcf3fa7d0abf4206aff5af
Ruba Egbaria <ruba.egb@gmail.com>
2023-02-16 23:24:40 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
+import { ExportComponent } from './export/export.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    NavigationBarComponent,
+    ExportComponent
   ],
   imports: [
     BrowserModule,
